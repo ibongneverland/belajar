6ffad0a81e5f24d09f6ace90276fb40c62aa8a4f
Adhe Kurniawan <ibongneverland@gmail.com>
2020-11-19 15:36:54 +0700

    Update and rename 18112020b2.txt to 18112020b2.php

--- 18112020b2.php
+++ 18112020b2.php
@@ -1,7 +1,11 @@
-sudo ls /etc/apt/sources.list.d
+<?php
+
+echo "sudo ls /etc/apt/sources.list.d
 
 Look for your desire PPA here and then remove the PPA using the following command:
 
 sudo rm -i /etc/apt/sources.list.d/PPA_Name.list
 
-hit 'y' then enter
+hit 'y' then enter"
+
+?>
