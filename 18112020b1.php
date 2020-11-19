8b0b73caa71aa1a87557e486082d06fa8ed3505c
Adhe Kurniawan <ibongneverland@gmail.com>
2020-11-19 16:12:16 +0700

    Update 18112020b1.php

--- 18112020b1.php
+++ 18112020b1.php
@@ -1,9 +1,11 @@
 <?php
 
 echo "Tutorial ini untuk install q4os linux 32bit di laptop HP mini 311.<br><br>
+
 Persiapan<br>
 1. Laptop HP mini 311 32bit RAM 1GB<br>
 2. File ISO q4os //peringatan: beda versi kemungkinan akan beda alur. Versi saat pembuatan tutorial ini adalah versi 3.12 (Centaurus)<br><br>
+
 Proses<br>
 1. Burn ISO ke flash disk dengan balenaEtcher<br>
 2. Boot Laptop dengan klik Esc saat laptop booting<br>
@@ -18,7 +20,6 @@ Proses<br>
 11. sudo apt install kde-plasma-desktop<br>
 12. sudo apt install plasma-nm<br>
 13. sudo apt install qt5-style-kvantum qt5-style-kvantum-themes<br>
-14. install tema dan aplikasi lain melalui kde market<br>
-";
+14. install tema dan aplikasi lain melalui kde market<br>";
 
 ?>
