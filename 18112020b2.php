7c7f269ecb59567528d3a96d023712187b47081b
Adhe Kurniawan <ibongneverland@gmail.com>
2020-11-19 16:11:41 +0700

    Update 18112020b2.php

--- 18112020b2.php
+++ 18112020b2.php
@@ -1,7 +1,9 @@
 <?php
-echo "sudo ls /etc/apt/sources.list.d<br><br>
-Look for your desire PPA here and then remove the PPA using the following command:<br><br>
+
+echo "sudo ls /etc/apt/sources.list.d<br><br>Look for your desire PPA here and then remove the PPA using the following command:<br><br>
+
 sudo rm -i /etc/apt/sources.list.d/PPA_Name.list<br><br>
-hit 'y' then enter<br><br>
-";
+
+hit 'y' then enter<br><br>";
+
 ?>
