b56df0a7103f8b6ea14c567d91c0320789534b48
Adhe Kurniawan <ibongneverland@gmail.com>
2020-11-19 15:35:49 +0700

    Update 19112020b3.php

--- 19112020b3.php
+++ 19112020b3.php
@@ -1,5 +1,5 @@
 <?php
 
-ALTER TABLE `rsl2020`.`template_laboratorium` echo "ADD CONSTRAINT `template_laboratorium_ibfk_1` FOREIGN KEY (`kd_jenis_prw`) REFERENCES `rsl2020`.`jns_perawatan_lab` (`kd_jenis_prw`) ON DELETE CASCADE ON UPDATE CASCADE;";
+echo "ALTER TABLE `rsl2020`.`template_laboratorium` ADD CONSTRAINT `template_laboratorium_ibfk_1` FOREIGN KEY (`kd_jenis_prw`) REFERENCES `rsl2020`.`jns_perawatan_lab` (`kd_jenis_prw`) ON DELETE CASCADE ON UPDATE CASCADE;";
 
 ?>
