17625df3d00339b48eb77be33c89eaf196a079b3
Adhe Kurniawan <ibongneverland@gmail.com>
2020-11-19 15:38:14 +0700

    Update 18112020b2.php

--- 18112020b2.php
+++ 18112020b2.php
@@ -6,6 +6,6 @@ Look for your desire PPA here and then remove the PPA using the following comman
 
 sudo rm -i /etc/apt/sources.list.d/PPA_Name.list
 
-hit 'y' then enter"
+hit 'y' then enter";
 
 ?>
